5428b28e046383347b052e94f92192d2726b2a25
Ghulam Akbar <150150845+akbar909@users.noreply.github.com>
2025-04-22 09:21:42 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://podcast-vert.vercel.app',
     methods: ['GET', 'POST']
   }
 });
